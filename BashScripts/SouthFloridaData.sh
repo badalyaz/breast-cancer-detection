0295be75fc02312e723101f4fec118fcfe67d66f
server <server@careaware.de>
2022-09-29 18:55:38 +0400

    addedtDd owDodnloading bash scripts

---
+++ BashScripts/SouthFloridaData.sh
@@ -0,0 +1,84 @@
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/normals/normal_02 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/DBA
+echo --------------------------------normal_02--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/normals/normal_03 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/DBA
+echo --------------------------------normal_03--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/normals/normal_04 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/DBA
+echo --------------------------------normal_04--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/normals/normal_05 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/DBA
+echo --------------------------------normal_05--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/normals/normal_06 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/DBA
+echo --------------------------------normal_06--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/normals/normal_07 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------normal_07--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/normals/normal_08 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------normal_08--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/normals/normal_09 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/LUMYSIS
+echo --------------------------------normal_09--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/normals/normal_10 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/LUMYSIS
+echo --------------------------------normal_10--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/normals/normal_11 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------normal_11--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/normals/normal_12 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------normal_12--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/cancers/cancer_01 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/LUMISYS
+echo --------------------------------cancer_01--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/cancers/cancer_02 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/LUMISYS
+echo --------------------------------cancer_02--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/cancers/cancer_03 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/DBA
+echo --------------------------------cancer_03--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/cancers/cancer_04 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/DBA
+echo --------------------------------cancer_04--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/cancers/cancer_05 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/LUMISYS
+echo --------------------------------cancer_05--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/cancers/cancer_06 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------cancer_06--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/cancers/cancer_07 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------cancer_07--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/cancers/cancer_08 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------cancer_08--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/cancers/cancer_09 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/LUMISYS
+echo --------------------------------cancer_09--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/cancers/cancer_10 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------cancer_10--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/cancers/cancer_11 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------cancer_11--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/cancers/cancer_12 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------cancer_12--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/cancers/cancer_13 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------cancer_13--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/cancers/cancer_14 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------cancer_14--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/cancers/cancer_15 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/LUMISYS
+echo --------------------------------cancer_15--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/benigns/benign_01 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/LUMISYS
+echo --------------------------------benign_01--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/benigns/benign_02 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------benign_02--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/benigns/benign_03 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------benign_03--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/benigns/benign_04 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/LUMISYS
+echo --------------------------------benign_04--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/benigns/benign_05 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------benign_05--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/benigns/benign_06 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/LUMISYS
+echo --------------------------------benign_06--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/benigns/benign_07 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------benign_07--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/benigns/benign_08/ -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------benign_08--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/benigns/benign_09/ -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------benign_09--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/benigns/benign_10/ -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------benign_10--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/benigns/benign_11/ -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------benign_11--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/benigns/benign_12 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/HOWTEK
+echo --------------------------------benign_12--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/benigns/benign_13/ -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/LUMISYS
+echo --------------------------------benign_13--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/benigns/benign_14/ -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/LUMISYS
+echo --------------------------------benign_14--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/benign_without_callbacks/benign_without_callback_01 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/LUMISYS
+echo --------------------------------bwc_01--------------------------------
+wget -m ftp://figment.csee.usf.edu/pub/DDSM/cases/benign_without_callbacks/benign_without_callback_02 -P /media/server/Linux_files/breast_cancer/Data/SFUniversity/LUMISYS
+echo --------------------------------bwc_02--------------------------------
