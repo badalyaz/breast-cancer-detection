78ade01d22ef6576768443d863f64dc6878c03eb
server <server@careaware.de>
2022-09-29 10:07:59 +0400

    Changed the location of the README file and added image to it

---
+++ DataProcessing/SFUniversity_Dataset/README.md
@@ -0,0 +1,36 @@
+## Purpose of this utility
+In this directory you can find some tools for converting the old LJPEG images of [SFUniversity's dataset](http://www.eg.usf.edu/cvprg/Mammography/Database.html) into png images
+This utility is for converting the old LJPEG images to png.
+
+1. The [DDSMUtility](https://github.com/badalyaz/cancer_detection/tree/interns_branch/DataProcessing/SFUniversity_Dataset/LJPEG2PNG_Converter/DDSMUtility) is the modern version of an [old code writen by SFUniversity](http://www.eng.usf.edu/cvprg/Mammography/software/heathusf_v1.1.0.html) written in matlab.
+2. There is a []
+3. [check_PNGcount.py](https://github.com/badalyaz/cancer_detection/blob/interns_branch/DataProcessing/SFUniversity_Dataset/check_PNGcount.py) file which checks whether all the png files were converted or not
+4. The [get_SFUniversity_Masks_Boxes.py](https://github.com/badalyaz/cancer_detection/blob/interns_branch/DataProcessing/SFUniversity_Dataset/get_SFUniversity_Masks_Boxes.py) file gets all the annotations from .ics and .OVERLAY files in the 
+   
+   
+
+![Test](https://github.com/badalyaz/cancer_detection/blob/interns_branch/DataProcessing/SFUniversity_Dataset/test.png "Some examples from the SFUniversity dataset")
+
+
+## Requirements
+- A Windows based machine
+- WinSCP
+- Cygwin (32 bit)
+- MATLAB >= 2010
+
+## Usage Details
+
+## Credits
+Special thanks for DDSMUtility to
+@misc{Anmol2015,
+  author = {Anmol Sharma},
+  title = {DDSM Utility},  
+  year = {2015},  
+  publisher = {GitHub},  
+  journal = {GitHub repository},  
+  howpublished = {\url{https://github.com/trane293/DDSMUtility}},  
+}
+
+
+
+
--- DataProcessing/SFUniversity_Dataset/check_PNGcount.py
+++ DataProcessing/SFUniversity_Dataset/check_PNGcount.py
@@ -3,37 +3,50 @@
 '''
 
 from glob import glob
-pngs = glob("/home/server/other_projects/breast_cancer/DATA_PATH/Data/SFUniversity/*/*/*/*/*/*/*/*/PNGFiles/*")
-ljpegs = glob("/home/server/other_projects/breast_cancer/DATA_PATH/Data/SFUniversity/*/*/*/*/*/*/*/*/PNGFiles/../*.LJPEG")
+from os import dup
+
+## Taking the paths to all of the .png and .ljpeg images in the SFU dataset
+pngs = glob("/path-to-your-png-images/SFUniversity/*/*/*/*/*/*/*/*/PNGFiles/*")
+ljpegs = glob("/path-to-your-LJPEG-images/SFUniversity/*/*/*/*/*/*/*/*/PNGFiles/../*.LJPEG")
 pngs = list(set(pngs))
 
 
-print(len(pngs),len(ljpegs))
+print(f"Comparing {len(pngs)} '.png' files with {len(ljpegs)} '.LJPEG' files")
 
-
+## Creating empty lists where the missing data, checked data and duplicates will be stored
 missing = []
 checked = []
 duplicates = []
+
+
+## Comparing all the pathes and filenames
 for ljpeg in ljpegs:
+    #Extracting the LJPEG filename and its case folder path from its path
     ljpeg_filename = ljpeg[ljpeg.rfind("/")+1:ljpeg.rfind(".")]
     ljpeg_case_folder = ljpeg[:ljpeg.rfind("/PNGFiles")]
 
-    b=False
+    are_same=False
     for png in pngs:
+        #Extracting the png filename and its case folder path from its path
         png_filename = png[png.rfind("/")+1:png.rfind(".")]
         png_case_folder = png[:png.rfind("/PNGFiles")]
+
+        #Checking if they are the same
         if ljpeg_filename == png_filename and ljpeg_case_folder==png_case_folder:
-            # print(f"{ljpeg_filename}:{png_filename}")
             if (png in checked):
+                #If the image was already checked, then its a duplicate
                 duplicates.append(png)
             else:
+                #If not, then mark is as checked
                 checked.append(png)
-            b=True
+            are_same=True
             break
-    
-    if not b:
-        # missing.append(ljpeg_case_folder+"/PNGFiles/"+ljpeg_filename+".png")
+    #If not the same add value to 'missing' list
+    if not are_same:
         missing.append(ljpeg_case_folder+"/"+ljpeg_filename+".LJPEG")
 
-print(f"These png files are missing --> {missing}\n\n")
-print(f"These files have duplicates --> {duplicates}")
+if len(missing) == 0 and len(duplicates)==0:
+    print("Everything is ok, all the images are converted")
+else:
+    print(f"png images for these files are missing --> {missing}\n\n")
+    print(f"These files are duplicates --> {duplicates}")
